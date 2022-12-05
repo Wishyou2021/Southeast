af076ef46d72d0a2cce957c1dc2e64fa7a8bd365
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2022-12-06 00:23:59 +0800

    Update Southeast.py

--- Southeast.py
+++ Southeast.py
@@ -1,11 +1,17 @@
 import streamlit as st  
-import ATLANTIC 
+import SOUTHEAST 
 st.title('NBA數據查詢系統')
 st.sidebar.header('選擇球隊及數據')
-option = st.sidebar.selectbox( '選擇分組？', ['ATLANTIC', '中央組', '東南組', '西北組','太平洋組','西南組'])
-if option=='ATLANTIC':
-  teams=st.sidebar.selectbox( '選擇球隊？', ['Boston Celtics', 'Brooklyn Nets', 'New York Knicks', 'Philadelphia 76ers','Toronto Raptors'])
-  if teams=='Boston Celtics':
-    ATLANTIC.BostonCeltics()
-  if teams=='Brooklyn Nets':
-    ATLANTIC.BrooklynNets()
+option = st.sidebar.selectbox( '選擇分組？', ['ATLANTIC', '中央組', 'SOUTHEAST', '西北組','太平洋組','西南組'])
+if option=='SOUTHEAST':
+  teams=st.sidebar.selectbox( '選擇球隊？', ['Atlanta Hawks', 'Charlotte Hornets', 'Miami Heat', 'Orlando Magic','Washington Wizards'])
+  if teams=='Atlanta Hawks':
+    SOUTHEAST.AtlantaHawks()
+  if teams=='Charlotte Hornets':
+    SOUTHEAST.CharlotteHornets()
+  if teams=='Miami Heat':
+    SOUTHEAST.MiamiHeat()
+  if teams=='Orlando Magic':
+    SOUTHEAST.OrlandoMagic()
+  if teams=='Washington Wizards':
+    SOUTHEAST.WashingtonWizards()
