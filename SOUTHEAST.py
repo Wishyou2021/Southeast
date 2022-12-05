e336009aa5f3f4dfd72bd36c6fa1d5dcac0b693a
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2022-12-06 00:43:24 +0800

    Update SOUTHEAST.py

--- SOUTHEAST.py
+++ SOUTHEAST.py
@@ -3,18 +3,18 @@ from PIL import Image
 def AtlantaHawks():
   col1, col2 = st.columns(2)
   with col1:
-    image = Image.open('teams logo/BostonCeltics.png')
+    image = Image.open('Atlanta_Hawks.png')
     st.image(image) 
   with col2:
      st.title('Atlanta Hawks')
-     st.subheader('老闆:Wyc Grousbeck')
-     st.subheader('GM:Juka Mcehaic')
-     st.subheader('總教練:Joe Mazzulla (臨時)')     
-  st.write('波士頓塞爾提克(1946年-至今)') 
-  st.write('波士頓塞爾蒂克隊的英文隊名為Boston Celtics，成立於1946年，目前所在地區是美國麻塞諸塞州波士頓市，主場為TD北岸花園球館，為美國職籃史上獲得總冠軍次數最多的球隊。')
+     st.subheader('老闆:Tony Ressler')
+     st.subheader('GM:Landry Fields')
+     st.subheader('總教練:Nate McMillan')     
+  st.write('水牛城野牛（1946年）三城黑鷹（1946年-1951年）密爾瓦基老鷹（1951年-1955年）聖路易斯老鷹（1955年-1968年）亞特蘭大老鷹（1968年-至今）') 
+  st.write('亞特蘭大老鷹的英文隊名為Atlanta Hawks，老鷹隊之名象徵著速度和進攻，球隊成立於1946年，在1949年加入NBA，是NBA的元老之一。目前所在城市是美國喬治亞州亞特蘭大市(Atlanta, Georgia)，主場為飛利浦球場(Philips Arena)。球隊最初名為「三城黑鷹隊」，1951年主場遷移到密爾瓦基市，改名「密爾瓦基鷹隊」，直到1968年主場移到亞特蘭大後才改成現在的亞特蘭大老鷹隊。')
   col1, col2= st.columns(2)
-  col1.metric("聯盟冠軍🏆", "22  次")
-  col2.metric("分組冠軍🏆", "32  次")   
+  col1.metric("聯盟冠軍🏆", "4  次")
+  col2.metric("分組冠軍🏆", "1  次")   
 def BrooklynNets():
   st.header('Brooklyn Nets')
   image = Image.open('teams logo/BrooklynNets.png')
