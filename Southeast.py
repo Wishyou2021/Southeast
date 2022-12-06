218c5c27cccc4aca5baba6f2f74dbf0bb26ed787
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2022-12-06 21:32:25 +0800

    Update Southeast.py

--- Southeast.py
+++ Southeast.py
@@ -7,11 +7,16 @@ if option=='SOUTHEAST':
   teams=st.sidebar.selectbox( '選擇球隊？', ['Atlanta Hawks', 'Charlotte Hornets', 'Miami Heat', 'Orlando Magic','Washington Wizards'])
   if teams=='Atlanta Hawks':
     SOUTHEAST.AtlantaHawks()
+    SOUTHEAST_map.AtlantaHawks_map()
   if teams=='Charlotte Hornets':
     SOUTHEAST.CharlotteHornets()
+    SOUTHEAST_map.CharlotteHornets_map()
   if teams=='Miami Heat':
     SOUTHEAST.MiamiHeat()
+    SOUTHEAST_map.MiamiHeat_map()
   if teams=='Orlando Magic':
     SOUTHEAST.OrlandoMagic()
+    SOUTHEAST_map.OrlandoMagic_map()
   if teams=='Washington Wizards':
     SOUTHEAST.WashingtonWizards()
+    SOUTHEAST_map.WashingtonWizards_map()
