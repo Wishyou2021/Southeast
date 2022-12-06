e8a14ed838cde673592e264b4fc0bf87ca0a823c
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2022-12-06 23:36:15 +0800

    Update Southeast.py

--- Southeast.py
+++ Southeast.py
@@ -1,5 +1,5 @@
 import streamlit as st  
-import SOUTHEAST 
+import SOUTHEAST_map
 st.title('NBA數據查詢系統')
 st.sidebar.header('選擇球隊及數據')
 option = st.sidebar.selectbox( '選擇分組？', ['ATLANTIC', '中央組', 'SOUTHEAST', '西北組','太平洋組','西南組'])
